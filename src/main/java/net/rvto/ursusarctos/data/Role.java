c008eaed1be5559b5352bcbec459bd30297dcc0c
Richard Townsend <richard.v.townsend@gmail.com>
2024-04-08 13:06:23 -0300

    Initial scaffolding from code generated at start.vaadin.com

---
+++ src/main/java/net/rvto/ursusarctos/data/Role.java
@@ -0,0 +1,5 @@
+package net.rvto.ursusarctos.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/net/rvto/ursusarctos/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package net.rvto.ursusarctos.data;
+
+import org.springframework.lang.NonNullApi;
